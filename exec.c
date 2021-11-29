f5827fb94da4324e1ac162f24785b07f35562171
Evan Tranvouez <91433114+DiorroerStlenneg@users.noreply.github.com>
2021-11-29 23:30:25 +0000

    The exec
    This program may not solve world hunger, but it can probably list the inside of a directory.

---
+++ exec.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+
+
+
+String dewIt(char *cmd, char **argv)
+{   pid_t simba;    //Simba is the state of our fork, the code returned by the function.
+    simba=fork()
+    if (simba==-1){
+        perror("fork failed");
+        return 1;
+    }
+    if (simba==0)   // Is child
+    {
+        execvp(cmd, argv);
+        printf("The child didn't graduate, it's a failure.\n");   // This will execute only in case of exec failure,
+		return 1;                           // Since exec replaces a processus' pile.
+    }
+    else {      // Is parent
+        waitpid(simba, &status, WUNTRACED); // Waits for a status report from the child. WUNTRACED is only here in case we use SIG actions in further development.
+    }
+    
+    
+    
+}
