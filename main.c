369d0f318f462183ce6957ec79540cd37ac95bc9
Yanis <Volgare@protonmail.com>
2021-12-02 16:33:09 +0100

    Début de l'assemblage du Shell

--- main.c
+++ main.c
@@ -7,15 +7,17 @@ int main()
     int flag = 1;
     char input[100];
     char working_dir[100];
+    char temp;
 
-    while (flag == 1)
+    while (flag == 1) //tant que l'utilisateur n'a pas dit qu'il voulait quitter on reste dans le programme
     {
         getcwd(working_dir, sizeof(working_dir));
         printf("%s$ ", working_dir);
-        scanf("%s", input);
+        scanf("%[^\n]", input);     //je lis jusqu'à ce que j'appuie sur Entrée
+        scanf("%c", &temp);     //je stoque le Entrée quelque part pour qu'il évite de faire des carabistouilles ailleur (sans cette line c'est un peu n'importe quoi)
         // printf("%s\n", input); //this has been used for debug
         // i hate life because switch case can't be used with strings
-        if (strcmp(input, "exit") == 0)
+        if (strcmp(input, "exit") == 0)     // l'utilisateur veut quitter donc fin du programme
         {
             flag = 0;
         }
@@ -24,4 +26,5 @@ int main()
     return 0;
 
     // this whole thing really is a base. The most difficult things will be how do we make all of these commands?
+    //note pour le l'amélioration : lorsque
 }
