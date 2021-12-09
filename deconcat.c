415e6c83b33e90658f3142baea1f204b5998c4bf
Yanis <Volgare@protonmail.com>
2021-12-09 22:27:18 +0100

    Deconcat.c est presque prêt, encore deux trois ajustement est c'est bon

--- deconcat.c
+++ deconcat.c
@@ -3,30 +3,33 @@
 #include <stdlib.h>
 
 char* deconcatCmd (char* commande){ //fonction qui permet à partir d'un tableau de caractères, de le déconcaténer en plusieurs tableaux de caractère eux meme dans un tableau.
-    char *res, *res2;
-    int i=0;
+    char *token, **res; //res est un pointeur sur pointeur sur char qui va devenir tableau
+    int nbmots=0, j;
     int nbCara = strlen(commande); //nombre de caractère de la commande
     printf("%d \n", nbCara);       //J'ai bien le nombre de caractères de ma commande.
     //printf("%s", commande );    //J'arrive à avoir la commande et à l'afficher ce qui est deja bien
-    res2 = (char*) malloc (nbCara * sizeof(char));
+    res = (char**) malloc (nbCara * sizeof(char)); // J'alloue a mon pointeur sur pointeur la taille necessaire
 
-    res = strtok(commande, " ");
-    //printf("%s", res);
+    
+    token = strtok(commande, " "); // initialisation de strtok
+    
 
-    while (res != NULL){
-        printf("%s", res);
-
+    while (token != NULL){ // tant qu'il reste des mots
+        res[nbmots]=token;
+        token = strtok(NULL, " "); //première lettre du mot suivant
+        nbmots++;
     }
-
-    //for (i=0; i<nbCara; i++){
-    //    if (commande[i])
-    //}
+    
+    
+    for (j=0; j<nbmots; j++){
+        printf("%s ", res[j]);
+    }
 
 }
 
 int main(){     //pour le débuggage
     printf("Hello \n");
-    char test[] = "yes No";
+    char test[] = "yes No je mange des pates aux saumons du crous";
     deconcatCmd(test);
     return 0;
 }
