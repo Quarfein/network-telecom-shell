b4ff524ee62328b445b23eb4a13feea977f8acc1
Evan Tranvouez <91433114+DiorroerStlenneg@users.noreply.github.com>
2022-01-09 23:17:28 +0000

    ls simple, basique
    Ne permet que le ls sans options pour l'instant, mais en vrai le -a se fait facile et le -l à voir avec sys/stat.h. Aussi à voir pour ls sans nom de fichier si on utilise pwd ou getenv pour check le repertoire courant

---
+++ ls.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <dirent.h>
+
+/* Pour aider à comprendre, voici la composition d'une structure dirent :
+struct dirent {
+    ino_t          d_ino;       --> inode number 
+    off_t          d_off;       --> offset to the next dirent 
+    unsigned short d_reclen;    --> length of this record 
+    unsigned char  d_type;      --> type of file; not supported by all file system types 
+    char           d_name[256]; --> filename 
+    }; */
+
+
+void ls_function(int argc,char *argv[]){
+   DIR *dir;            // le receptacle temporaire du fichier
+   struct dirent *dent;
+   dir = opendir(argv[1]);   // A changer si l'on veut rajouter les options car va prendre le 2e argument direct pour nom de rep
+
+    if(dir!=NULL){
+        while((dent=readdir(dir))!=NULL){ //readdir renvoie donc un pointeur vers une structure dirent, donc un tableau de tous les éléments du rép
+            if((strcmp(dent->d_name,".")==0 || strcmp(dent->d_name,"..")==0 || (*dent->d_name) == '.' )){   // le rep courant, rep père et fichiers cachés sont ignorés. 
+            }
+       else
+              {
+        printf(dent->d_name);
+        printf("\n");
+              }
+                    }
+       }
+       close(dir);
+     }
