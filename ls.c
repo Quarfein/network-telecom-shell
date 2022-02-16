5d338112d15d309b66e4e3e49736c2a4afcc3733
Yanis <Volgare@protonmail.com>
2022-02-16 16:47:57 +0100

    Correction de l'indentation

--- ls.c
+++ ls.c
@@ -15,33 +15,31 @@ struct dirent {
 
 
 void ls_function(int argc,char *argv[]){
-   DIR *dir;            // le receptacle temporaire du fichier
-   struct dirent *dent;
-   char temp[UCHAR_MAX];
-   if(argv[1]!=NULL || argv[1][0]!="-"){
-    dir = opendir(argv[1]);   // A changer si l'on veut rajouter les options car va prendre le 2e argument direct pour nom de rep
-   }
-   else{
-    if (getcwd (temp, UCHAR_MAX) == NULL) {
-        perror("failed to get current directory\n"); 
-     }
-       else{
-          dir = opendir(temp);
-   }
-   }
-   
-   if(dir!=NULL){
-      while((dent=readdir(dir))!=NULL){ //readdir renvoie donc un pointeur vers une structure dirent, donc un tableau de tous les éléments du ré
-          if((strcmp(dent->d_name,".")==0 || strcmp(dent->d_name,"..")==0 || (*dent->d_name) == '.' )){   // le rep courant, rep père et fichiers cachés sont ignorés. 
-                }
-            else
-                {
-        printf(dent->d_name);
-        printf("\n");
-              }
-                    }
-       }
-       close(dir);
-     }
     
+    DIR *dir;            // le receptacle temporaire du fichier
+    struct dirent *dent;
+    char temp[UCHAR_MAX];
+    if(argv[1]!=NULL || argv[1][0]!="-"){
+        dir = opendir(argv[1]);   // A changer si l'on veut rajouter les options car va prendre le 2e argument direct pour nom de rep
+    }
+    else{
+        if (getcwd (temp, UCHAR_MAX) == NULL) {
+            perror("failed to get current directory\n"); 
+        }
+        else{
+            dir = opendir(temp);
+        }
+    }
+   
+    if(dir!=NULL){
+        while((dent=readdir(dir))!=NULL){ //readdir renvoie donc un pointeur vers une structure dirent, donc un tableau de tous les éléments du ré
+            if((strcmp(dent->d_name,".")==0 || strcmp(dent->d_name,"..")==0 || (*dent->d_name) == '.' )){   // le rep courant, rep père et fichiers cachés sont ignorés. 
+            }
+            else {
+                printf(dent->d_name);
+                printf("\n");
+            }
+        }
+    }
+    close(dir);
 }
